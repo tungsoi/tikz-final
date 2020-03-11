c748fea861cc29713236c8d5a258cae8e0cdb6ab
tungsoi <thanhtung.atptit@gmail.com>
2020-03-11 09:37:49 +0700

    check view share

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\ConfigSite;
 use App\Models\MenuHome;
 use App\Models\Slide;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,14 +29,40 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $pictureCategories = Category::all();
-        $pictureTags = Tag::all();
-        $configSites = ConfigSite::all();
-        $menus = MenuHome::whereStatus(1)->orderBy('order', 'asc')->get();
+        if (Schema::hasTable('categories')) {
+            $pictureCategories = Category::all();
+        } else {
+            $pictureCategories = null;
+        }
+
+        if (Schema::hasTable('tags')) {
+            $pictureTags = Tag::all();
+        } else {
+            $pictureTags = null;
+        }
+
+        if (Schema::hasTable('config_sites')) {
+            $configSites = ConfigSite::all();
+        } else {
+            $configSites = null;
+        }
+
+        if (Schema::hasTable('menu_homes')) {
+            $menus = MenuHome::whereStatus(1)->orderBy('order', 'asc')->get();
+        } else {
+            $menus = null;
+        }
+
+        if (Schema::hasTable('slides')) {
+            $slides = Slide::orderBy('order')->get();
+        } else {
+            $slides = null;
+        }
+
         view()->share('pictureCategories', $pictureCategories);
         view()->share('pictureTags', $pictureTags);
         view()->share('configSites', $configSites);
         view()->share('menus', $menus);
-        view()->share('slides', Slide::orderBy('order')->get());
+        view()->share('slides', $slides);
     }
 }

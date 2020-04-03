754963952f1acba485b170642513e48fb5c07957
tungsoi <thanhtung.atptit@gmail.com>
2020-04-03 14:44:36 +0700

    create banner

--- resources/views/admin/tikz/frontend/layouts/banner.blade.php
+++ resources/views/admin/tikz/frontend/layouts/banner.blade.php
@@ -1,16 +1,25 @@
 <div class="banner">
-    <div class="row">
-        <div class="col-sm-7">
+    <div class="row no-mg-bottom">
+        <div class="col-sm-12">
             <div class="bannerLeft">
-                <a href="#">
-                    <a href="/"><img src="{{ asset('tikz/img/images/tikz_logo_new.png') }}" title="Tạp chí Tikz @ 2017" alt="" /></a>
-                </a>
+                <div class="bannerRow bannerRowBg1"></div>
+                <div class="bannerRow bannerRowBg2"></div>
+                <div class="bannerRow bannerRowBg3">
+                    <p class="slogan">Elegant Simplicity</p>
+                </div>
+                <div class="bannerLogo no-padding">
+                    <div class="col-xs-2 col-xs-offset-5 no-padding h100">
+                        <div class="logoRadius">
+                            <p class="bannerLogoSlogan">tikZ.vn</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-sm-5">
+        {{-- <div class="col-sm-5">
             <div class="bannerRight">
                 <a href="{{ route('admin.login') }}" class="Đăng nhập" title="Đăng nhập">+ Đăng nhập</a>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
